90f2545a416499a4bd4bf6ce37db0623a68f8711
Takudzwa Kamamba <161172263+Takudzwa-Kamamba@users.noreply.github.com>
2024-03-21 18:44:38 +0100

    Update StringConcatenate.java

--- src/StringConcatenate.java
+++ src/StringConcatenate.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class StringConcatenate {
 
     public static void main(String[] args) {
+        
         // Create a Scanner object to read input from the user
         Scanner scanner = new Scanner(System.in);
 
